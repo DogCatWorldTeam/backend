ae7f37877758d28bf936177eeb9cc7dca640d5e8
MJJ <m3k0813@gmail.com>
2024-07-10 15:53:34 +0900

    feat: Cors DNS 설정

--- src/main/java/com/techeer/abandoneddog/global/config/CorsConfig.java
+++ src/main/java/com/techeer/abandoneddog/global/config/CorsConfig.java
@@ -16,6 +16,7 @@ public class CorsConfig {
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:5173");
         config.addAllowedOrigin("http://15.164.85.218:5173");
+        config.addAllowedOrigin("http://dogcatworld.site:5173");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);

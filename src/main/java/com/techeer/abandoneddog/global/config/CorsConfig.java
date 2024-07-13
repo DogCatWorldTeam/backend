714b8fa397599c8bc22da9eb8f1e147a5730cc11
MJJ <m3k0813@gmail.com>
2024-07-13 14:02:24 +0900

    fix: Cors 오류 수정

--- src/main/java/com/techeer/abandoneddog/global/config/CorsConfig.java
+++ src/main/java/com/techeer/abandoneddog/global/config/CorsConfig.java
@@ -17,9 +17,8 @@ public class CorsConfig {
         config.addAllowedOrigin("http://localhost:5173");
         config.addAllowedOrigin("http://15.164.85.218:5173");
         config.addAllowedOrigin("http://dogcatworld.site:5173");
-        config.addAllowedOrigin("http://dogcatworld.site:");
+        config.addAllowedOrigin("http://dogcatworld.site");
         config.addAllowedOrigin("http://192.168.123.136:5173");
-
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);

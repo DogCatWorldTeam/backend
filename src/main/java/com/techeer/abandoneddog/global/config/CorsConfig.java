deb90423bc841a1dc72f0f6d7309b7e57fa046c7
MJJ <m3k0813@gmail.com>
2024-07-15 13:20:45 +0900

    style: global 코드 포메팅

--- src/main/java/com/techeer/abandoneddog/global/config/CorsConfig.java
+++ src/main/java/com/techeer/abandoneddog/global/config/CorsConfig.java
@@ -9,20 +9,20 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5173");
-        config.addAllowedOrigin("http://15.164.85.218:5173");
-        config.addAllowedOrigin("http://dogcatworld.site:5173");
-        config.addAllowedOrigin("http://dogcatworld.site:");
-        config.addAllowedOrigin("http://192.168.123.136:5173");
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("http://localhost:5173");
+		config.addAllowedOrigin("http://15.164.85.218:5173");
+		config.addAllowedOrigin("http://dogcatworld.site:5173");
+		config.addAllowedOrigin("http://dogcatworld.site:");
+		config.addAllowedOrigin("http://192.168.123.136:5173");
 
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
 }
